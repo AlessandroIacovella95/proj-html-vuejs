a8b80370ee5cefc6abacd7594aace31815d088bc
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-09-19 15:08:04 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
+import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
+library.add(faStarSolid, faStarRegular, faBoltLightning);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
